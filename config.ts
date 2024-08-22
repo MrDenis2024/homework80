1f723522acf5fa455f87615e011c6b3fb34c06c8
Khrunev Denis <deniskhrunev@gmail.com>
2024-08-22 12:38:43 +0600

    Создана структура подключения к базе данных mysql

---
+++ config.ts
@@ -0,0 +1,11 @@
+
+const config = {
+  database: {
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'office',
+  },
+};
+
+export default config;
--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import categoriesRouter from './routes/categories';
 import locationsRouter from './routes/locations';
 import itemsRouter from './routes/items';
+import mysqlDb from './mysqlDb';
 
 const app = express();
 const port = 8000;
@@ -11,6 +12,12 @@ app.use('/categories', categoriesRouter);
 app.use('/locations', locationsRouter);
 app.use('/items', itemsRouter);
 
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
+const run = async () => {
+  await mysqlDb.init();
+
+  app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+  });
+};
+
+run().catch(console.error);
